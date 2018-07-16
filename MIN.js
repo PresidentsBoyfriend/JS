bfbe849eb92178d500f20776b4654cbbaad5d2e9
PresidentsBoyfriend <41293675+PresidentsBoyfriend@users.noreply.github.com>
2018-07-16 17:12:44 +0300

    Add files via upload

---
+++ MIN.js
@@ -0,0 +1,12 @@
+function min(a,b) {
+	if (a<b){
+		alert('a<b');
+	} else if (a>b){
+		alert ('a>b');
+	} else alert ('a=b');
+}
+
+var a,b;
+a = prompt ('Enter a = ',''); 
+b = prompt ('Enter b = ','');
+min(a,b);
