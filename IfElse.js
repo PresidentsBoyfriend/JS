66972f02b6405650ef9e5ce8d758145ee4fd9ca3
PresidentsBoyfriend <41293675+PresidentsBoyfriend@users.noreply.github.com>
2018-07-16 15:45:46 +0300

    Add files via upload

---
+++ IfElse.js
@@ -0,0 +1,8 @@
+var nameJS;
+nameJS = prompt ('Введите второе официальное название JavaScript?', 'Не знаю.');
+if (nameJS='ECMAScript')
+{
+	alert ('Да, правильно!');
+} else {
+	alert ('Неверно!');
+}
